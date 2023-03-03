465640cfc32507ffb78c025ba78a320b24c0a9a9
Joshua-alt <mbavazijoshua@gmail.com>
2023-03-03 15:38:16 +0100

    add pack

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const request = require('request-promise');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 const app = express();
 app.use(express.json());
 
